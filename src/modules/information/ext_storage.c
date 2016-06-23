b4bc179bf861f458ea17664f87a0960fce16451e
Radoslaw Czerski <r.czerski@samsung.com>
2016-06-23 14:33:21 +0200

    information/ext_storage: Showing icon fixed.
    Fix for TSAM-5234
    
    SD card icon is now showed after inserting card when device is on.
    
    Change-Id: I8f78709ba260bc8b8812cf38d03d9f4e42514904
    Signed-off-by: Radoslaw Czerski <r.czerski@samsung.com>

--- src/modules/information/ext_storage.c
+++ src/modules/information/ext_storage.c
@@ -21,6 +21,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <storage.h>
+#include <vconf.h>
+
 #include "common.h"
 #include "indicator.h"
 #include "main.h"
@@ -30,7 +32,6 @@
 
 #define ICON_PRIORITY	INDICATOR_PRIORITY_SYSTEM_3
 #define MODULE_NAME		"mmc"
-#define TIMER_INTERVAL	0.3
 
 static int register_ext_storage_module(void *data);
 static int unregister_ext_storage_module(void);
@@ -56,24 +57,23 @@ typedef enum {
 	EXT_STORAGE_STATE_UNMOUNTABLE = 0x08,
 } ext_storage_state_e;
 
-int state = 0x00;
-
 static const char *icon_path[] = {
 	"Storage/B03_storage_t_flash.png",
 	NULL
 };
+
 static int updated_while_lcd_off = 0;
 static int bShown = 0;
 
 
-static void set_app_state(void* data)
+static void set_app_state(void *data)
 {
 	ext_storage.ad = data;
 }
 
 static void show_image_icon(void)
 {
-	if(bShown == 1)
+	if (bShown == 1)
 		return;
 
 	ext_storage.img_obj.data = icon_path[0];
@@ -89,55 +89,35 @@ static void hide_image_icon(void)
 	bShown = 0;
 }
 
-static void _ext_storage_cb (int storage_id, storage_state_e state, void *data)
+static void _ext_storage_cb(keynode_t *node, void *user_data)
 {
-	int *status = (int *)data;
-
 	_D("indicator_mmc_change_cb");
+
+	int storage_status;
+
 	if(icon_get_update_flag() == 0) {
 		updated_while_lcd_off = 1;
 		return;
 	}
+
 	updated_while_lcd_off = 0;
 
-	switch (state) {
-		case STORAGE_STATE_MOUNTED_READ_ONLY:
-		case STORAGE_STATE_MOUNTED:
-			*status = *status | EXT_STORAGE_STATE_MOUNTED;
-			if (*status & EXT_STORAGE_STATE_REMOVED)
-				*status = *status - EXT_STORAGE_STATE_REMOVED; // remove bit LSB
+	vconf_get_int(VCONFKEY_SYSMAN_MMC_STATUS, &storage_status);
+
+	switch (storage_status) {
+		case VCONFKEY_SYSMAN_MMC_MOUNTED:
+			show_image_icon();
 			break;
 
-		case STORAGE_STATE_UNMOUNTABLE:
-		case STORAGE_STATE_REMOVED:
-			*status = *status | EXT_STORAGE_STATE_REMOVED;
-			if (*status & EXT_STORAGE_STATE_MOUNTED)
-				*status = *status - EXT_STORAGE_STATE_MOUNTED; // remove bit second LSB
+		case VCONFKEY_SYSMAN_MMC_INSERTED_NOT_MOUNTED:
+		case VCONFKEY_SYSMAN_MMC_REMOVED:
+			hide_image_icon();
 			break;
 
 		default:
-			_D("Storage with ID:%d, Invalid state:%d", storage_id, state);
+			_E("Invalid state:%d", storage_status);
 			break;
 	}
-}
-
-static void change_indicator_state(int storage_state)
- {
-	_D("Storage indicator icon set");
-
-	if ((storage_state & EXT_STORAGE_STATE_MOUNTED) || (storage_state & EXT_STORAGE_STATE_MOUNTED_READ_ONLY))
-		show_image_icon();
-
-	else
-		hide_image_icon();
- }
-
-static bool _wake_up_storage_register_cb(int storage_id, storage_type_e type, storage_state_e state, const char *path, void *user_data)
-{
-	if (type == STORAGE_TYPE_EXTERNAL)
-		_ext_storage_cb(storage_id, state, user_data);
-
-	return true;
 }
 
 static int wake_up_cb(void *data)
@@ -145,23 +125,9 @@ static int wake_up_cb(void *data)
 	if(updated_while_lcd_off == 0)
 		return OK;
 
-	int ret = storage_foreach_device_supported(_wake_up_storage_register_cb, (void *)&state);
-	retvm_if(ret != STORAGE_ERROR_NONE, FAIL, "storage_foreach_device_supported failed[%s]", get_error_message(ret));
-
-	change_indicator_state(state);
+	_ext_storage_cb(NULL, data);
 
 	return OK;
-}
-
-static bool _storage_register_cb(int storage_id, storage_type_e type, storage_state_e state, const char *path, void *user_data)
-{
-	if (type == STORAGE_TYPE_EXTERNAL) {
-		_ext_storage_cb(storage_id, state, user_data);
-		int ret = storage_set_state_changed_cb(storage_id, _ext_storage_cb, user_data);
-		retvm_if(ret != STORAGE_ERROR_NONE, false, "storage_foreach_device_supported failed[%s]", get_error_message(ret));
-	}
-
-	return true;
 }
 
 static int register_ext_storage_module(void *data)
@@ -171,34 +137,17 @@ static int register_ext_storage_module(void *data)
 
 	set_app_state(data);
 
-	state = EXT_STORAGE_STATE_REMOVED;
+	ret = vconf_notify_key_changed(VCONFKEY_SYSMAN_MMC_STATUS, _ext_storage_cb, data);
+	retvm_if(ret == FAIL, FAIL, "vconf_notify_key_changed failed!");
 
-	ret = storage_foreach_device_supported (_storage_register_cb, (void *)&state);
-
-	retvm_if(ret != STORAGE_ERROR_NONE, FAIL, "storage_foreach_device_supported failed[%s]", get_error_message(ret));
-
-	change_indicator_state(state);
+	_ext_storage_cb(NULL, data);
 
 	return OK;
 }
 
-static bool _storage_unregister_cb(int storage_id, storage_type_e type, storage_state_e state, const char *path, void *user_data)
-{
-	int ret;
-	if (type == STORAGE_TYPE_EXTERNAL) {
-		ret = storage_unset_state_changed_cb(storage_id, _ext_storage_cb);
-		if(ret != STORAGE_ERROR_NONE)
-			_D("storage_unset_state_changed_cb failed[%s]", get_error_message(ret));
-	}
-	return true;
-}
-
 static int unregister_ext_storage_module(void)
 {
-	int ret;
-
-	ret = storage_foreach_device_supported (_storage_unregister_cb, NULL);
-	retvm_if(ret != STORAGE_ERROR_NONE, FAIL, "storage_foreach_device_supported failed[%s]", get_error_message(ret));
+	vconf_ignore_key_changed(VCONFKEY_SYSMAN_MMC_STATUS, _ext_storage_cb);
 
 	return OK;
 }
